36952fda58d360839bb065b8f18d227fbef7dd52
Francis Kinyae <fmkinyae@gmail.com>
2021-07-25 16:41:55 +0300

    add an instance of the returned promise

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Users } from '../users';
 import {  UsersService } from "../users.service";
 
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css'],
-  providers: [ UsersService ]
+  styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
 
-  constructor(private gUsers: UsersService) { }
+  users:any = Users;
+  
+
+
+  constructor(private userService:  UsersService) {
+    
+   }
 
   ngOnInit(): void {
+    
   }
 
   doSearch(username: string){
-this.gUsers.getUsers(username);
+ this.userService.getUsers(username);
+ this.users = this.userService.users;
+this.ngOnInit();
   }
 
 }
